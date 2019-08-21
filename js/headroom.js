045a326253e6dba7458a10718d8f645c34f6a4c3
0And1Story <witherman233@gmail.com>
2019-08-21 21:04:10 +0800

    add headroom

--- js/headroom.js
+++ js/headroom.js
@@ -2,6 +2,43 @@ function getScrollProgress() {
 	let winTop = document.documentElement.scrollTop || document.body.scrollTop;
 	let docHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
 	let winHeight = document.documentElement.clientHeight || document.body.clientHeight;
-    let progress = winTop / (docHeight-winHeight) * 100;
+    let progress = {
+        'percent': winTop / (docHeight-winHeight) * 100,
+        'pixel': winTop
+    };
     return progress;
 }
+
+function updateHeader() {
+    let progress = getScrollProgress();
+    if (progress.pixel > 840 + 64) {
+        let header = document.getElementsByClassName('mdui-appbar')[0];
+        header.setAttribute('style', 'background-color: white;');
+        let header_a = header.getElementsByTagName('a');
+        for (let i = 0; i < header_a.length; i ++) {
+            header_a[i].setAttribute('style', 'color: black !important;')
+        }
+        let btn = header.getElementsByClassName('mdui-btn');
+        for (let i = 0; i < btn.length; i ++) {
+            btn[i].classList.add('mdui-ripple');
+        }
+    } else {
+        let header = document.getElementsByClassName('mdui-appbar')[0];
+        header.setAttribute('style', '');
+        let header_a = header.getElementsByTagName('a');
+        for (let i = 0; i < header_a.length; i ++) {
+            header_a[i].setAttribute('style', '')
+        }
+        let btn = header.getElementsByClassName('mdui-btn');
+        for (let i = 0; i < btn.length; i ++) {
+            btn[i].classList.remove('mdui-ripple');
+        }
+    }
+}
+
+(function() {
+    window.addEventListener('load', function() {
+        updateHeader();
+        window.addEventListener('scroll', updateHeader);
+    });
+})();
